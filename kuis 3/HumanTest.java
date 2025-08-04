138c7857f4c273d9c54707f078590239834a9520
kkh4yy <khayra.tazkiya@ui.ac.id>
2025-08-04 17:47:26 +0700

    Add files via upload

---
+++ kuis 3/HumanTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class HumanTest {
+    @Test
+    void testCompareTo() {
+        assertEquals;
+        assertEquals;
+    }
+
+    @Test
+    void testGetNama() {
+        assertEquals;
+        assertEquals;
+    }
+
+    @Test
+    void testToString() {
+        assertEquals;
+        assertEquals;
+    }
+}
