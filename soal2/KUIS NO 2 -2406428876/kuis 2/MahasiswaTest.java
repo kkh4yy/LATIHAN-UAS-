c7d472c3a75f4fef76b952194832773a3d823926
kkh4yy <khayra.tazkiya@ui.ac.id>
2025-08-04 17:48:07 +0700

    Add files via upload

---
+++ soal2/KUIS NO 2 -2406428876/kuis 2/MahasiswaTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+public class MahasiswaTest {
+    @Test
+    void testGetAngkatan() {
+
+    }
+
+    @Test
+    void testGetIPK() {
+
+    }
+
+    @Test
+    void testGetNama() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
